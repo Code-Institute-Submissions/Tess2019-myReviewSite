a0c906e0eea7bf0ed0ae71f90d3314ab806fa7fe
Tess2019 <terese_burman@hotmail.com>
2021-01-02 17:52:19 +0000

    added base.html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
   import  env 
 
 
 app = Flask(__name__)
+# to finding documents from mongdb
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
+# check if it works
 @app.route("/")
 def hello():
     return "Hello World!"
